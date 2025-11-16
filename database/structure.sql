3ffd96e09281efdc334e81fdee2577fdbb133927
Reiisoficial <wandersonhrluz@gmail.com>
2025-11-15 22:14:23 -0300

    fix: remover comentários desnecessários do structure.sql

--- database/structure.sql
+++ database/structure.sql
@@ -76,6 +76,4 @@ INSERT INTO classificacao_cdd (codigo_cdd, descricao) VALUES
 
 CREATE DATABASE IF NOT EXISTS sabi_academico;
 USE sabi_academico;
--- Add your safe table structures here
--- WITHOUT any sensitive credentials or real da
 
